7ab059f40cfa8c296edcc4370f094d9528988b43
lihuan <anyueleo@163.com>
2023-04-19 17:57:04 +0800

    ✨ feat: 判断api调用

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,14 @@
-/*
- * @Author: lihuan
- * @Date: 2023-04-18 14:31:45
- * @LastEditors: lihuan
- * @LastEditTime: 2023-04-18 14:31:51
- * @Description: 
- */
+import { app } from 'framework';        // import app 定义
+
+const dataLen = 3;
+let name = 'iceman';
+
+function doWell () {
+    const app =4;                       // 局部常量 app 定义
+    return app;                         // 局部常量 app 调用
+}
+
+function getInfos (info: string) {
+    const result = app.get(info);       // import app 调用 
+    return result;
+}
--- src/test/test1/test5.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: lihuan
- * @Date: 2023-04-18 15:04:47
- * @LastEditors: lihuan
- * @LastEditTime: 2023-04-18 15:04:52
- * @Description:
- */
